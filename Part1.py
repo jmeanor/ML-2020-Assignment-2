7320859d3b91cdfd37fc28a842f65125c6a8eedb
John Meanor <jmeanor@gmail.com>
2020-02-16 22:59:29 -0500

    Encapsulating Part1 class for a framework for better testing.

--- Part1.py
+++ Part1.py
@@ -1,5 +1,6 @@
 import mlrose_reborn as mlrose
 import numpy as np
+import pydash as _ 
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
@@ -10,26 +11,28 @@ from sklearn.metrics import accuracy_score
 # fitness = mlrose.Queens()
 fitness = mlrose.FourPeaks(t_pct=0.15)
 
-# Solve using simulated annealing - attempt 1         
+# Solve using simulated annealing - attempt 1
 # init_state = np.array([0, 1, 2, 3, 4, 5, 6, 7])
 init_state = np.array([1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0])
 
 # Define optimization problem object
-problem = mlrose.DiscreteOpt(length = len(init_state), fitness_fn = fitness, maximize=False, max_val=len(init_state))
+problem = mlrose.DiscreteOpt(length=len(
+    init_state), fitness_fn=fitness, maximize=False, max_val=len(init_state))
 
 # Define decay schedule
 schedule = mlrose.ExpDecay()
 
-class Part1(): 
+
+class Part1():
     # Tutorial from MLRose Documentation
     # https://github.com/gkhayes/mlrose/blob/master/tutorial_examples.ipynb
-    
-    def __init__(self, fitness = fitness, init_state=init_state, problem=problem, schedule=schedule):
+
+    def __init__(self, fitness=fitness, init_state=init_state, problem=problem, schedule=schedule):
         # Initialize fitness function object using pre-defined class
         # fitness = mlrose.Queens()
         self.fitness = fitness
 
-        # Solve using simulated annealing - attempt 1         
+        # Solve using simulated annealing - attempt 1
         # init_state = np.array([0, 1, 2, 3, 4, 5, 6, 7])
         self.init_state = init_state
 
@@ -39,63 +42,64 @@ class Part1():
         # Define decay schedule
         self.schedule = schedule
 
-    # Setup 
+    # Setup
     # def setup(self, fitness = fitness, init_state=init_state, problem=problem, schedule=schedule):
     #     fitness = fitness
 
     # Random Hill-Climbing
     def runRHC(self):
-        best_state, best_fitness, _ = mlrose.random_hill_climb(self.problem, max_attempts = 10, max_iters = np.inf, init_state = self.init_state,
-                                                            random_state = 1)
+        default = {
+            'problem': self.problem,
+            'max_attempts': 10, 
+            'max_iters': np.inf, 
+            'init_state': self.init_state,
+            'random_state': 1
+        }
+        self._rhc(**default)
+        
+        a = _.assign({}, default, {'max_iters': 10})
+        self._rhc(**a)
+        b = _.assign({}, default, {'max_iters': 20})
+        self._rhc(**b)
+
+
+
+    def _rhc(self, **kwargs):
         print('RHC Attempt 1 ')
-        print(best_state)
-        print(best_fitness)
 
-        best_state, best_fitness, _ = mlrose.random_hill_climb(self.problem, max_attempts = 30, max_iters = np.inf, init_state = self.init_state,
-                                                            random_state = 1)
-        print('RHC Attempt 2 ')
-        print(best_state)
-        print(best_fitness)
+        best_state, best_fitness, _ = mlrose.random_hill_climb( **kwargs )
 
-        best_state, best_fitness, _ = mlrose.random_hill_climb(self.problem, max_attempts = 5, max_iters = np.inf, init_state = init_state,
-                                                            random_state = 1)
-        print('RHC Attempt 3 ')
         print(best_state)
         print(best_fitness)
 
     # Simulated Annealing
     def runSA(self):
-        best_state, best_fitness, _  = mlrose.simulated_annealing(self.problem, schedule = self.schedule, max_attempts = 10, 
-                                                        max_iters = 1000, init_state = init_state,
-                                                        random_state = 1)
+        best_state, best_fitness, _ = mlrose.simulated_annealing(self.problem, schedule=self.schedule, max_attempts=10,
+                                                                 max_iters=1000, init_state=init_state,
+                                                                 random_state=1)
         print('SA Attempt 1 ')
         print(best_state)
         print(best_fitness)
 
-        best_state, best_fitness, _  = mlrose.simulated_annealing(self.problem, schedule = self.schedule, max_attempts = 10, 
-                                                        max_iters = 2000, init_state = init_state,
-                                                        random_state = 1)
+        best_state, best_fitness, _ = mlrose.simulated_annealing(self.problem, schedule=self.schedule, max_attempts=10,
+                                                                 max_iters=2000, init_state=init_state,
+                                                                 random_state=1)
         print('SA Attempt 2 ')
         print(best_state)
         print(best_fitness)
 
     # Genetic Algorithms
     def runGA(self):
-        best_state, best_fitness, _ = mlrose.random_hill_climb(self.problem, max_attempts = 10, max_iters = np.inf, init_state = init_state,
-                                                            random_state = 1)
+        best_state, best_fitness, _ = mlrose.random_hill_climb(self.problem, max_attempts=10, max_iters=np.inf, init_state=init_state,
+                                                               random_state=1)
         print('GA Attempt 1 ')
         print(best_state)
         print(best_fitness)
 
     # Mimic
     def runMIMIC(self):
-        best_state, best_fitness, _ = mlrose.genetic_alg(self.problem, pop_size = 200, mutation_prob = 0.1, max_attempts = 10, max_iters = np.inf, random_state=1)
+        best_state, best_fitness, _ = mlrose.genetic_alg(
+            self.problem, pop_size=200, mutation_prob=0.1, max_attempts=10, max_iters=np.inf, random_state=1)
         print('MIMIC Attempt 1 ')
         print(best_state)
         print(best_fitness)
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from Part1 import Part1
-
+# import subprocess as sp
+# sp.call('clear',shell=True)
 
 def runPart1():
     part1 = Part1()
     print('Running Random Hill-Climb...')
     part1.runRHC()
-    print('Running Simulated Annealing...')
-    part1.runSA()
-    print('Running Genetic Algorithm...')
-    part1.runGA()
-    print('Running MIMIC...')
-    part1.runMIMIC()
+    # print('Running Simulated Annealing...')
+    # part1.runSA()
+    # print('Running Genetic Algorithm...')
+    # part1.runGA()
+    # print('Running MIMIC...')
+    # part1.runMIMIC()
 
 # Main block
 runPart1()
